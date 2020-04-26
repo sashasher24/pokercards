488a8f3147a2b769922fd3c701174bab8d2eae20
Oleksandra Sherstiuk <osherstiuk@e2r5p7.unit.ua>
2020-04-27 00:12:59 +0300

    main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include "header.h"
 
 bool duplicate (char *cards[]) {
-    for (int i = 0; i <= 4; i++) {
-        for (int j = i+1; j <= 4; j++) {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = i+1; j <= 5; j++) {
             if (!mx_strcmp(cards[i],cards[j])) {
                 mx_printerr("Duplicate cards: ");
 		mx_printerr(cards[i]);
@@ -15,7 +15,7 @@ bool duplicate (char *cards[]) {
 }
 
 void inval_card(char *cards[]) {
-    int i = 0;
+    int i = 1;
     bool rank = 0;
     bool suit = 0;
     while (cards[i] != '\0') {
